bb417d5a366a1c178524bb3d7d4e0c5e40e79d4b
Amagi:DDmxh <amagiddmxh@gmail.com>
2023-03-06 23:22:36 +0800

    Use docs route as the default docs page

--- apps/web/components/nav/navigations.ts
+++ apps/web/components/nav/navigations.ts
@@ -1,4 +1,4 @@
 export const headerNvigations = [
-  ["Docs", "https://aa-lib-docs.vercel.app/"],
+  ["Docs", "https://aa-lib-docs.vercel.app/documents/1-1-problems"],
   ["Weekly News", "https://github.com/accountjs/aa-research"],
 ]
