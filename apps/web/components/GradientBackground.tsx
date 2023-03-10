0c93ba9c70cd916522875b9f079325b641bd3172
Amagi:DDmxh <amagiddmxh@gmail.com>
2023-03-11 04:06:09 +0800

    Fix expanded width on mobile, polish

--- apps/web/components/GradientBackground.tsx
+++ apps/web/components/GradientBackground.tsx
@@ -33,14 +33,14 @@ export const GradientBackground = () => {
 
   return (
     <div
-      className={`absolute top-0 left-0 h-full w-full z-[-2] bg-[#fc7b7b5e]`}
+      className="fixed top-0 left-0 h-full w-full z-[-2] bg-[#fc7b7b5e]"
       style={{
         background: `radial-gradient(at center, ${gradientFromColor}, ${gradientToColor})`,
       }}
       ref={gradientRef}
     >
-      <div className="absolute z-0 w-[757px] h-[567px] left-[calc(50%-628px/2-170px)] top-2 bg-[rgba(253,92,156,.5)] opacity-20 filter blur-[150px]" />
-      <div className="absolute z-0 w-[759px] h-[776px] left-10 bottom-0 bg-[rgba(135,105,255,.5)] opacity-30 filter blur-[150px]" />
+      <div className="fixed z-0 w-[757px] h-[567px] left-[calc(50%-628px/2-170px)] top-2 bg-[rgba(253,92,156,.5)] opacity-20 filter blur-[150px]" />
+      <div className="fixed z-0 w-[759px] h-[776px] left-10 bottom-0 bg-[rgba(135,105,255,.5)] opacity-30 filter blur-[150px]" />
     </div>
   )
 }
